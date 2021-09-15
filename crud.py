11413edc6232962d14a30e6bf01e0ec51f19136e
konsgrib <konstantin@gribanov.net>
2021-09-15 16:29:10 +0300

    schemas.User class is fixd

--- crud.py
+++ crud.py
@@ -11,10 +11,17 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 
+def create_user(db: Session, user: schemas.User):
+    db_user = models.User(**user.dict())
+    print("First name: " + db_user.first_name)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+    
 # def create_user(db: Session, user: schemas.UserCreate):
 #     db_user = models.User(id=user.id)
 #     db.add(db_user)
 #     db.commit()
 #     db.refresh(db_user)
 #     return db_user
-    
--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 
 # create new user
 @app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+def create_user(user: schemas.User, db: Session = Depends(get_db)):
     db_user = crud.get_user(db,  user_id=user.id )
     if db_user:
         raise HTTPException(status_code=400, detail="User already registered")
