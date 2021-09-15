b1a624226af5ae12fafb28b589712790e5d63017
konsgrib <konstantin@gribanov.net>
2021-09-15 16:05:43 +0300

    schemas.User class is fixd

--- crud.py
+++ crud.py
@@ -11,10 +11,10 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 
-def create_user(db: Session, user: schemas.UserCreate):
-    db_user = models.User(id=user.id)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+# def create_user(db: Session, user: schemas.UserCreate):
+#     db_user = models.User(id=user.id)
+#     db.add(db_user)
+#     db.commit()
+#     db.refresh(db_user)
+#     return db_user
     
