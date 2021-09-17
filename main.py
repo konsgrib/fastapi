abf775f3724f3edef64f6273f8e1af79149e97b6
konsgrib <konstantin@gribanov.net>
2021-09-17 17:20:50 +0300

    OAuth2 simple token is added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List,Optional
 
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
@@ -8,6 +8,8 @@ import models
 import schemas
 from database import SessionLocal, engine
 import uvicorn
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -15,6 +17,21 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(debug=True)
 
+
+# ==================================
+
+oauth2_scheme= OAuth2PasswordBearer(tokenUrl='token')
+
+# @app.post('/token')
+async def token(form_data: OAuth2PasswordRequestForm = Depends()):
+    return {'access_token':form_data.username + '_token'}
+
+
+
+@app.get('/index')
+async def index(token: str = Depends(oauth2_scheme)):
+    return {'auth_token': token}
+# ==================================
 
 # Dependency
 def get_db():
@@ -27,7 +44,7 @@ def get_db():
 
 # get user
 @app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
+def read_user(user_id: int, db: Session = Depends(get_db),token: str = Depends(oauth2_scheme)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -36,7 +53,7 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 
 #get users
 @app.get("/users/", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db),token: str = Depends(oauth2_scheme)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
@@ -44,7 +61,7 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 
 # create new user
 @app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.User, db: Session = Depends(get_db)):
+def create_user(user: schemas.User, db: Session = Depends(get_db),token: str = Depends(oauth2_scheme)):
     db_user = crud.get_user(db,  user_id=user.id )
     if db_user:
         raise HTTPException(status_code=400, detail="User already registered")
@@ -54,7 +71,7 @@ def create_user(user: schemas.User, db: Session = Depends(get_db)):
 
 #update or create user
 @app.post("/users/{user_id}", response_model=schemas.User)
-def update_user(user: schemas.User, db: Session = Depends(get_db)):
+def update_user(user: schemas.User, db: Session = Depends(get_db),token: str = Depends(oauth2_scheme)):
     db_user = crud.get_user(db,  user_id=user.id )
     if db_user:
         return crud.update_user(db=db,user=user)
@@ -64,7 +81,7 @@ def update_user(user: schemas.User, db: Session = Depends(get_db)):
 
 
 @app.delete("/users/{user_id}", response_model=str)
-def del_user(user_id: int, db: Session = Depends(get_db)):
+def del_user(user_id: int, db: Session = Depends(get_db),token: str = Depends(oauth2_scheme)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user:
         return crud.del_user(db=db,user_id=db_user.id)
